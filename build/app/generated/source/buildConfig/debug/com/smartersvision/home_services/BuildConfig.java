8c9a79e65b3938413107b7ff71fa6c4618c2f9ec
evill <villegase@detodito.net>
2022-11-25 11:00:56 -0400

    first commit

---
+++ build/app/generated/source/buildConfig/debug/com/smartersvision/home_services/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.smartersvision.home_services;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.smartersvision.home_services";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 31;
+  public static final String VERSION_NAME = "2.2.2";
+}
